c488c87fdd8241e25b97384b18a935010b66d55c
windy <windylucky@yeah.net>
2017-11-28 22:08:47 +0800

    enjoy

---
+++ src/main/java/com/framework/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/11/28.
+ */
+package com.framework;
---
+++ src/main/java/com/framework/utils/SpringContext.java
@@ -0,0 +1,22 @@
+package com.framework.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Created by Administrator on 2017/11/28.
+ */
+public class SpringContext implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+
+        return applicationContext.getBean(clazz);
+    }
+}
