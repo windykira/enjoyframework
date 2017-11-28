6d858fcad51bf3cac900b5ce7d4cdd4b21d6bd3b
windy <windylucky@yeah.net>
2017-11-28 22:07:57 +0800

    enjoy

---
+++ src/main/java/com/framework/utils/MyObject.java
@@ -0,0 +1,7 @@
+package com.framework.utils;
+
+/**
+ * Created by Administrator on 2017/11/28.
+ */
+public class MyObject {
+}
---
+++ src/main/java/com/framework/utils/TestClass.java
@@ -0,0 +1,12 @@
+package com.framework.utils;
+
+/**
+ * Created by Administrator on 2017/11/28.
+ */
+public class TestClass {
+
+    public void test(){
+
+        MyObject myObject = SpringContext.getBean("maneger");
+    }
+}
