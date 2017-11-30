2a0979872f26171e59df66f8aaac06464ac079ff
windy <windylucky@yeah.net>
2017-11-30 22:20:19 +0800

    enjoy

--- src/main/java/com/framework/utils/SpringContext.java
+++ src/main/java/com/framework/utils/SpringContext.java
@@ -10,9 +10,20 @@ import org.springframework.context.ApplicationContextAware;
 public class SpringContext implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
+    private SpringContext springContext;
+    private static final Object instanceLocker = new Object();
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+
+        synchronized (instanceLocker) {
+            if (springContext == null) {
+                springContext = applicationContext.getBean(SpringContext.class);
+                springContext.applicationContext = applicationContext;
+            } else if (applicationContext != springContext.applicationContext) {
+                throw new RuntimeException("ObjectContext 初始化错误：在当前运行环境中检测到多个不同的 Spring 上下文。");
+            }
+
+        }
     }
 
     public static <T> T getBean(Class<T> clazz) {
